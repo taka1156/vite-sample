fa07977aed72f4421e10cac2cdf7f92f1e8432b3
taka <tomo05061@gmail.com>
2021-02-22 00:02:30 +0900

    1st commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 本番時はgithubリポジトリをルートパスにする
+  base: process.env.NODE_ENV === 'production' ? '/vite-sample/' : './',
+  resolve: {
+    // @/ によるimportをできるようにする。
+    alias: {
+      '@': path.resolve('__dirname', '/src')
+    }
+  },
+  build: {
+    outDir: 'docs'
+  },
+  plugins: [vue()]
+});
