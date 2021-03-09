4aad16d8f3c1cc86311f91c8875e5555453bf201
taka <tomo05061@gmail.com>
2021-03-09 23:36:16 +0900

    fix: プロジェクト名の変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 本番時はgithubリポジトリをルートパスにする
-  base: process.env.NODE_ENV === 'production' ? '/vite-sample/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/vite-sample-vue/' : './',
   resolve: {
     // @/ によるimportをできるようにする。
     alias: {
